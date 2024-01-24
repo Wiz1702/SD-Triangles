191f2e0527bac7f9b119f89688bdcf1158ffcfb8
Wiz1702 <151788806+Wiz1702@users.noreply.github.com>
2024-01-24 14:50:19 -0500

    Update triangles.rb

--- triangles.rb
+++ triangles.rb
@@ -1,1 +1,12 @@
 puts "Kenyon for Wisdom"
+def square_area(side_length)
+  area = side_length**2
+  return area
+end
+
+# Example: Find the area of a square with side length 5
+side_length = 5
+area = square_area(side_length)
+
+puts "The area of the square with side length #{side_length} is: #{area}"
+  
